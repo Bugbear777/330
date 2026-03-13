9119ce21435d88b10b885a93738c4f7e254d6e0e
Joseph-Marlow <jmarlow@byupathway.edu>
2026-03-13 12:27:03 -0700

    Week 2 team assignment - dynamic product page

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,12 @@
+import { getParam } from "./utils.mjs";
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   let cartItems = getLocalStorage("so-cart") || [];
